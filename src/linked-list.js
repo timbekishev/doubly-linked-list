36b31735e8f65d9c9e36dae7468fd5c88b2845f1
Timofei <btv.97@bk.ru>
2017-02-05 11:40:15 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,34 +2,48 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-	this.length = 0;
+		this.length = 0;
         this._head = null;
         this._tail = null;	
 	}
 
-    append(data) {}
-
-    head() {
-		
-        
-		
-	}
-
-    tail() {
-		
+    append(data) { // добавление в конец
 	
 	}
 
-    at(index) {}
+    head() {
+		if (this._head != null)
+    	return this._head.data;
+    	else
+    		return null; 
+	}
+
+    tail() {
+		if (this._tail != null)
+    		return this._tail.data;
+    	else
+    		return null;
+	}
+
+    at(index) { // возвращает данные по индексу
+		
+		
+	}
 
     insertAt(index, data) {}
 
     isEmpty() {
-			
+		if (this.lenght == 0) 
+			return true 
+		else 
+			return false;			
 	}
 
     clear() {
-			
+		this.length = 0;
+        this._head = null;
+        this._tail = null;
+        return this;		
 	}
 
     deleteAt(index) {}
